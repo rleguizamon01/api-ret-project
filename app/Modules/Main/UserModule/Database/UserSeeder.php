9b38c59c2a84e72dfbe770152fbc06cf4f80a89d
Rodolfo Leguizamon <leguizamon.rodolfo@gmail.com>
2022-02-27 21:58:00 -0300

    users created from seeder

--- app/Modules/Main/UserModule/Database/UserSeeder.php
+++ app/Modules/Main/UserModule/Database/UserSeeder.php
@@ -3,7 +3,10 @@
 namespace App\Modules\Main\UserModule\Database;
 
 use App\Modules\Main\UserModule\User;
+use Carbon\Carbon;
 use Database\Seeders\BaseSeeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends BaseSeeder
 {
@@ -11,6 +14,46 @@ class UserSeeder extends BaseSeeder
     {
         if(!$this->shouldRun()) return;
 
-        User::factory()->count(5)->create();
+        DB::table('users')->insert([
+            'first_name' => 'Retoño',
+            'last_name' => 'Admin',
+            'email' => 'admin@retono.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => null,
+            'deleted_at' => null,
+            'created_by' => null,
+            'updated_by' => null,
+            'deleted_by' => null
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Retoño',
+            'last_name' => 'Diseñador',
+            'email' => 'designer@retono.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => null,
+            'deleted_at' => null,
+            'created_by' => null,
+            'updated_by' => null,
+            'deleted_by' => null
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Retoño',
+            'last_name' => 'Dev',
+            'email' => 'dev@retono.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => null,
+            'deleted_at' => null,
+            'created_by' => null,
+            'updated_by' => null,
+            'deleted_by' => null
+        ]);
     }
 }
