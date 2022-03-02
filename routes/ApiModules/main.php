c6a5ea7b65c85ffb7c5a8fc03ea56e84ae21d72d
rleguizamon01 <leguizamon.rodolfo@gmail.com>
2022-03-02 02:08:27 -0300

    route heroku bugfix

--- routes/ApiModules/main.php
+++ routes/ApiModules/main.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->group(function() {
-    include('app/Modules/Main/UserModule/user-routes.php');
+    include(__DIR__ . '/../../app/Modules/Main/UserModule/user-routes.php');
 });
 
 Route::prefix('projects')->group(function() {
-    include('routes/ApiModules/projects.php');
+    include('../ApiModules/projects.php');
 });
 
 Route::prefix('forums')->group(function() {
-    include('routes/ApiModules/forums.php');
+    include('../ApiModules/forums.php');
 });
