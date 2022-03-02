66c5e4d7f0960a40ebf9b65487aae655c9923b60
rleguizamon01 <leguizamon.rodolfo@gmail.com>
2022-03-02 02:02:37 -0300

    route bugfix

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 include('ApiModules/public.php');
 
-include('app/Modules/Auth/AuthModule/auth-routes.php');
+include('../app/Modules/Auth/AuthModule/auth-routes.php');
 
 Route::group(['middleware' => 'auth:api'], function() {
 
     Route::prefix('main')->group(function () {
-        include('routes/ApiModules/main.php');
+        include('ApiModules/main.php');
     });
 });
