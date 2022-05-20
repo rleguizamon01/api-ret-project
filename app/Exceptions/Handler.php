87e9ed98f3f878bd8bcf5130edd854184b433922
rleguizamon01 <leguizamon.rodolfo@gmail.com>
2022-05-19 23:18:50 -0300

    error handler updated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +42,58 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // avoid showing authentication error
+        if($exception->getCode() != 6) {
+            Log::error('Error [' . $exception->getCode() . '] ocurrido en ' . $exception->getFile() . " (" . $exception->getLine() . ") por usuario " . (Auth::id() ?? '[no logeado]') . ' en endpoint ' . ($request->fullUrl() ?? '[no definido]') . ': ' . $exception->getMessage());
+            Log::error($exception->getTraceAsString());
+
+            if($request->has('password'))
+                unset($request['password']);
+
+            Log::info($request);
+        }
+
+        if($exception instanceof ValidationException) {
+            Log::error('validation errors: ', $exception->errors());
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'errors' => [
+                    'error' => 'No se han encontrado registros.'
+                ],
+                'model' => $exception->getModel()
+            ], 404);
+        }
+
+        if ($exception instanceof \Error) {
+            return response()->json([
+                'errors' => [
+                    'error' => 'Ha ocurrido un error de sistema.'
+                ]
+            ], 500);
+        }
+
+        if ($exception instanceof \BadMethodCallException) {
+            return response()->json([
+                'errors' => [
+                    'error' => 'Ha ocurrido un error de sistema.'
+                ]
+            ], 500);
+        }
+
+        if ($exception instanceof UnauthorizedException) {
+            return response()->json([
+                'errors' => [
+                    'error' => 'No posee permisos para realizar esta acción.'
+                ]
+            ], 403);
+        }
+
+        return parent::render($request, $exception);
     }
 }
